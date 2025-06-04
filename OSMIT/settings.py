5dbf09a7995ca3f8f04f587766c82e53ad304779
luisdrz20 <luisdavidrs002@gmail.com>
2025-06-04 12:42:30 -0600

    whitenoise2

--- OSMIT/settings.py
+++ OSMIT/settings.py
@@ -148,5 +148,10 @@ STATICFILES_DIRS = [
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#recolecta todo lo estatic
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''DESMADRE DEL PWA
